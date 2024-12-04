24739f5065f79500db966245c25da621dd02dac9
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2024-12-04 15:05:28 +0700

    Project skeleton

---
+++ target/debug/deps/zero2prod-5897f90b14b152dc.d
@@ -0,0 +1,5 @@
+C:\Users\masb0ymas\Documents\Repository\mamenesia\rust\zero2prod\target\debug\deps\libzero2prod-5897f90b14b152dc.rmeta: src/main.rs
+
+C:\Users\masb0ymas\Documents\Repository\mamenesia\rust\zero2prod\target\debug\deps\zero2prod-5897f90b14b152dc.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zero2prod-b0ada1fdba3a31dc.d
@@ -0,0 +1,5 @@
+C:\Users\masb0ymas\Documents\Repository\mamenesia\rust\zero2prod\target\debug\deps\libzero2prod-b0ada1fdba3a31dc.rmeta: src/main.rs
+
+C:\Users\masb0ymas\Documents\Repository\mamenesia\rust\zero2prod\target\debug\deps\zero2prod-b0ada1fdba3a31dc.d: src/main.rs
+
+src/main.rs:
