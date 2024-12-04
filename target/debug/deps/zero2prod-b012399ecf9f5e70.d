77400cd92ead816365e59ac3b8a07f7f90b721d6
Dedy Prasetyo Hermawan <dedy.prasetyo.h@gmail.com>
2024-12-04 15:32:13 +0700

    feat: setup actions

---
+++ target/debug/deps/zero2prod-b012399ecf9f5e70.d
@@ -0,0 +1,5 @@
+C:\Users\masb0ymas\Documents\Repository\mamenesia\rust\zero2prod\target\debug\deps\zero2prod-b012399ecf9f5e70.exe: src/main.rs
+
+C:\Users\masb0ymas\Documents\Repository\mamenesia\rust\zero2prod\target\debug\deps\zero2prod-b012399ecf9f5e70.d: src/main.rs
+
+src/main.rs:
